4b1f254b692a23d8d1e8705b6deea7b1df0c18af
Tomas Lopez <lopeztomasenrique98@gmail.com>
2021-08-28 14:43:03 -0300

    feat: Methods added to start working.

--- Date/Date.h
+++ Date/Date.h
@@ -4,12 +4,18 @@
 
 class Date
 {
+    private:
+        int relativeDay;
+
     public:
         Date();
+        Date(int day, int month, int year);
 
-    protected:
-
-    private:
+        Date addDays(int days) const;
+        void addDays(int days);
+        int differenceDays(const Date* date2) const;
+        void getDMY(int* day, int* month, int* year) const;
+        void setDMY(int day, int month, int year);
 };
 
 #endif // DATE_H
